7b47385d979554b6f28615b756c3493a79bac72e
gbabin <645219+gbabin@users.noreply.github.com>
2021-10-09 14:00:00 +0200

    parse received command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::str;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
@@ -14,11 +16,22 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer : [u8; 1024] = [0; 1024];
+fn handle_connection(stream: TcpStream) {
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
 
-    let n = stream.read(&mut buffer).unwrap();
+    let mut size_bytes : Vec<u8> = vec![];
+    reader.read_until(' ' as u8, &mut size_bytes).unwrap();
+    size_bytes.truncate(size_bytes.len() - 1);
+    let size : usize = str::from_utf8(&size_bytes).unwrap().parse::<usize>().unwrap();
+    println!("size = {}", size);
 
-    println!("n = {}", n);
-    println!("buffer = <{}>", String::from_utf8_lossy(&buffer[..]));
+    let mut command : Vec<u8> = vec![];
+    reader.read_until(' ' as u8, &mut command).unwrap();
+    command.retain(|&x| x != ' ' as u8 && x != 0);
+    let command_len = command.len();
+    println!("command = <{}>", String::from_utf8(command).unwrap());
+
+    let mut data : Vec<u8> = vec![0; size-command_len];
+    reader.read_exact(&mut data).unwrap();
+    println!("data = <{}>", String::from_utf8(data).unwrap());
 }

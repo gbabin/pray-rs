b43bf154309f6cc5bee03be6525c1e1c8d76220c
gbabin <645219+gbabin@users.noreply.github.com>
2021-10-10 15:00:00 +0200

    large resolutions

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use std::thread;
 use std::time;
 
 const GROUP_SIZE : u32 = 64;
-const GROUPS_COUNT : u32 = 8;
+const GROUPS_COUNT : u32 = 60; // 3840 × 2160 px
 
 const WINDOW_WIDTH : u32 = GROUP_SIZE * GROUPS_COUNT;
 const WINDOW_HEIGHT : u32 = (WINDOW_WIDTH * 9) / 16;
@@ -22,7 +22,7 @@ const WINDOW_HEIGHT : u32 = (WINDOW_WIDTH * 9) / 16;
 const XML_FILE : &str = "../scenes/testScene1.xml";
 
 const IMAGE_DATA_SIZE : usize = (WINDOW_WIDTH*WINDOW_HEIGHT*3) as usize;
-type ImageData = [u8; IMAGE_DATA_SIZE];
+type ImageData = Vec<u8>;
 
 const CLIENTS_COUNT : u32 = 6;
 
@@ -36,27 +36,25 @@ fn main() {
     encoder.set_color(png::ColorType::Rgb);
     encoder.set_depth(png::BitDepth::Eight);
     let mut writer_png = encoder.write_header().unwrap();
-    let shared_image = Arc::new(Mutex::new([127u8; IMAGE_DATA_SIZE]));
+    let image_data = vec![127u8; IMAGE_DATA_SIZE];
+    let shared_image = Arc::new(Mutex::new(image_data));
 
     let mut handles = vec![];
 
     println!("Listening...");
 
-    for i in 0..CLIENTS_COUNT {
-        match listener.accept() {
-            Ok((stream, _addr)) => {
-                println!("Connection established ({})", i);
-                
-                let shared_image = Arc::clone(&shared_image);
-                let handle = thread::spawn(move || {        
-                    handle_connection(stream, Arc::clone(&shared_image),
-                                      i*WINDOW_HEIGHT/CLIENTS_COUNT,
-                                      (i+1)*WINDOW_HEIGHT/CLIENTS_COUNT-1);
-                });
-                handles.push(handle);
-            }
-            Err(e) => println!("couldn't get client: {:?}", e),
-        }
+    for id in 1..CLIENTS_COUNT+1 {
+        let (stream, _addr) = listener.accept().unwrap();
+
+        println!(">>> [{}] Connection established", id);
+
+        let shared_image = Arc::clone(&shared_image);
+        let handle = thread::spawn(move || {
+            handle_connection(stream, id, Arc::clone(&shared_image),
+                              (id-1)*WINDOW_HEIGHT/CLIENTS_COUNT,
+                              id*WINDOW_HEIGHT/CLIENTS_COUNT-1);
+        });
+        handles.push(handle);
     }
 
     for handle in handles {
@@ -64,7 +62,7 @@ fn main() {
     }
 
     println!(">>> Saving image ...");
-    writer_png.write_image_data(&*shared_image.lock().unwrap()).unwrap();
+    writer_png.write_image_data(&**shared_image.lock().unwrap()).unwrap();
 }
 
 fn set_pixel(image: &mut [u8], x: u32, y: u32, r: u8, g: u8, b: u8) {
@@ -83,7 +81,7 @@ fn receive_command(reader: &mut BufReader<TcpStream>) {
     let mut data : Vec<u8> = vec![0; size+1];
     reader.read_exact(&mut data).unwrap();
     println!("data = <{}>", String::from_utf8_lossy(&data));
-    //println!("data = <{:?}>", data); 
+    //println!("data = <{:?}>", data);
 }
 
 fn receive_command_result(reader: &mut BufReader<TcpStream>, shared_image: Arc<Mutex<ImageData>>, y: u32) {
@@ -97,11 +95,11 @@ fn receive_command_result(reader: &mut BufReader<TcpStream>, shared_image: Arc<M
     reader.read_exact(&mut data).unwrap();
     //println!("data = <{}>", String::from_utf8_lossy(&data));
     //println!("data = <{:?}>", data);
-    
+
     let mut image = shared_image.lock().unwrap();
 
     for x in 0 .. WINDOW_WIDTH {
-        set_pixel(&mut *image, x, y,
+        set_pixel(&mut **image, x, y,
                   data[(9+3*x  ) as usize],
                   data[(9+3*x+1) as usize],
                   data[(9+3*x+2) as usize]);
@@ -116,22 +114,22 @@ fn send_command(writer: &mut BufWriter<TcpStream>, command: &str) {
     writer.flush().unwrap();
 }
 
-fn handle_connection(stream: TcpStream, shared_image: Arc<Mutex<ImageData>>, y_min: u32, y_max: u32) {
+fn handle_connection(stream: TcpStream, id: u32, shared_image: Arc<Mutex<ImageData>>, y_min: u32, y_max: u32) {
     let mut reader = BufReader::new(stream.try_clone().unwrap());
     let mut writer = BufWriter::new(stream);
 
     let second = time::Duration::from_secs(1);
 
-    println!(">>> Waiting LOGIN ...");
+    println!(">>> [{}] Waiting LOGIN ...", id);
     receive_command(&mut reader);
 
-    println!(">>> Sending INFO ...");
+    println!(">>> [{}] Sending INFO ...", id);
     let command_info = format!("INFO {} {}", WINDOW_WIDTH, WINDOW_HEIGHT);
     send_command(&mut writer, &command_info);
 
     thread::sleep(second);
 
-    println!(">>> Sending SETSCENE ...");
+    println!(">>> [{}] Sending SETSCENE ...", id);
     let command_info = format!("SETSCENE {}", XML_FILE);
     send_command(&mut writer, &command_info);
 
@@ -139,14 +137,14 @@ fn handle_connection(stream: TcpStream, shared_image: Arc<Mutex<ImageData>>, y_m
 
     for y in y_min .. y_max+1 {
 
-        println!(">>> Sending CALCULATE ({}/{}) ...", y+1, WINDOW_HEIGHT);
+        println!(">>> [{}] Sending CALCULATE ({}/{}) ...", id, y-y_min+1, y_max-y_min+1);
         let command_info = format!("CALCULATE {} {} {} {}", 1, WINDOW_HEIGHT-1-y, WINDOW_WIDTH, 1);
         send_command(&mut writer, &command_info);
 
-        println!(">>> Waiting CALCULATING ...");
+        println!(">>> [{}] Waiting CALCULATING ...", id);
         receive_command(&mut reader);
 
-        println!(">>> Waiting RESULT ...");
+        println!(">>> [{}] Waiting RESULT ...", id);
         let shared_image = Arc::clone(&shared_image);
         receive_command_result(&mut reader, shared_image, y);
     }

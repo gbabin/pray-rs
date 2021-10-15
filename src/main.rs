eab765f7ed90df7f4fcc6a653441e7c766fb8752
gbabin <645219+gbabin@users.noreply.github.com>
2021-10-15 21:00:00 +0200

    new protocol + timeout + thread names

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::path::Path;
 use std::str;
-use std::thread;
-use std::time;
+use std::time::Duration;
 
 const GROUP_SIZE : u32 = 64;
 const GROUPS_COUNT : u32 = 60; // 3840 × 2160 px
@@ -23,6 +22,8 @@ const XML_FILE : &str = "../scenes/testScene1.xml";
 const IMAGE_DATA_SIZE : usize = (WINDOW_WIDTH*WINDOW_HEIGHT*3) as usize;
 
 const CLIENTS_COUNT : u32 = 6;
+
+const CLIENT_COMPUTATION_TIMEOUT : u64 = 10;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
@@ -42,19 +43,24 @@ fn main() {
     let chunk_size : usize = (chunk_line_count * WINDOW_WIDTH * 3) as usize;
 
     crossbeam_utils::thread::scope(|sc| {
+
         for (i, image_chunk) in image_data.chunks_mut(chunk_size).enumerate() {
             let id : u32 = (i+1) as u32;
             let (stream, _addr) = listener.accept().unwrap();
+            stream.set_read_timeout(Some(Duration::from_secs(CLIENT_COMPUTATION_TIMEOUT))).unwrap();
 
             println!(">>> [{}] Connection established", id);
 
-            sc.spawn(move |_| {
-                handle_connection(stream, id, image_chunk,
-                                  (id-1)*chunk_line_count,
-                                  u32::min(id*chunk_line_count-1, WINDOW_HEIGHT-1));
-            });
+            sc.builder()
+                .name(format!("client_{}", id))
+                .spawn(move |_| {
+                    handle_connection(stream, id, image_chunk,
+                                      (id-1)*chunk_line_count,
+                                      u32::min(id*chunk_line_count-1, WINDOW_HEIGHT-1));
+                    println!(">>> [{}] Finished", id);
+            }).unwrap();
         }
-    }).unwrap();
+    }).expect("Failed to close client threads scope");
 
     println!(">>> Saving image ...");
     writer_png.write_image_data(&image_data).unwrap();
@@ -65,12 +71,12 @@ fn receive_command(reader: &mut BufReader<TcpStream>) {
     reader.read_until(' ' as u8, &mut size_bytes).unwrap();
     size_bytes.truncate(size_bytes.len() - 1);
     let size : usize = str::from_utf8(&size_bytes).unwrap().parse::<usize>().unwrap();
-    println!("size = {}", size);
+    // println!("size = {}", size);
 
     let mut data : Vec<u8> = vec![0; size+1];
     reader.read_exact(&mut data).unwrap();
-    println!("data = <{}>", String::from_utf8_lossy(&data));
-    //println!("data = <{:?}>", data);
+    // println!("data = <{}>", String::from_utf8_lossy(&data));
+    // println!("data = <{:?}>", data);
 }
 
 fn receive_command_result(reader: &mut BufReader<TcpStream>, image: &mut [u8], relative_y: u32) {
@@ -78,12 +84,12 @@ fn receive_command_result(reader: &mut BufReader<TcpStream>, image: &mut [u8], r
     reader.read_until(' ' as u8, &mut size_bytes).unwrap();
     size_bytes.truncate(size_bytes.len() - 1);
     let size : usize = str::from_utf8(&size_bytes).unwrap().parse::<usize>().unwrap();
-    //println!("size = {}", size);
+    // println!("size = {}", size);
 
     let mut data : Vec<u8> = vec![0; size+1];
     reader.read_exact(&mut data).unwrap();
-    //println!("data = <{}>", String::from_utf8_lossy(&data));
-    //println!("data = <{:?}>", data);
+    // println!("data = <{}>", String::from_utf8_lossy(&data));
+    // println!("data = <{:?}>", data);
 
     let (_header, pixels) = data.split_at(9);
     let (pixels, _zero) = pixels.split_at(pixels.len()-1);
@@ -103,37 +109,32 @@ fn send_command(writer: &mut BufWriter<TcpStream>, command: &str) {
     writer.flush().unwrap();
 }
 
-fn handle_connection(stream: TcpStream, id: u32, image: &mut [u8], y_min: u32, y_max: u32) {
+fn handle_connection(stream: TcpStream, _id: u32, image: &mut [u8], y_min: u32, y_max: u32) {
     let mut reader = BufReader::new(stream.try_clone().unwrap());
     let mut writer = BufWriter::new(stream);
 
-    let second = time::Duration::from_secs(1);
+    // println!(">>> [{}] Waiting LOGIN ...", id);
+    receive_command(&mut reader); // LOGIN
 
-    println!(">>> [{}] Waiting LOGIN ...", id);
-    receive_command(&mut reader);
-
-    println!(">>> [{}] Sending INFO ...", id);
+    // println!(">>> [{}] Sending INFO ...", id);
     let command_info = format!("INFO {} {}", WINDOW_WIDTH, WINDOW_HEIGHT);
     send_command(&mut writer, &command_info);
 
-    thread::sleep(second);
+    receive_command(&mut reader); // INFODONE
 
-    println!(">>> [{}] Sending SETSCENE ...", id);
+    // println!(">>> [{}] Sending SETSCENE ...", id);
     let command_info = format!("SETSCENE {}", XML_FILE);
     send_command(&mut writer, &command_info);
 
-    thread::sleep(second);
+    receive_command(&mut reader); // SETSCENEDONE
 
-    for y in y_min .. y_max+1 {
+    for y in y_min ..= y_max {
 
-        println!(">>> [{}] Sending CALCULATE ({}/{}) ...", id, y-y_min+1, y_max-y_min+1);
+        // println!(">>> [{}] Sending CALCULATE ({}/{}) ...", id, y-y_min+1, y_max-y_min+1);
         let command_info = format!("CALCULATE {} {} {} {}", 1, y, WINDOW_WIDTH, 1);
         send_command(&mut writer, &command_info);
 
-        println!(">>> [{}] Waiting CALCULATING ...", id);
-        receive_command(&mut reader);
-
-        println!(">>> [{}] Waiting RESULT ...", id);
+        // println!(">>> [{}] Waiting RESULT ...", id);
         receive_command_result(&mut reader, image, y-y_min);
     }
 }
